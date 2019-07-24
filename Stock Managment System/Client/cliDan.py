ef0c38e4641f998d1c713b8a893894cd6ed76230
Daniel Wise <daniel@autosiliconehoses.com>
2019-07-24 15:38:04 +0100

    Small Client changes
    IP Address etc

--- Stock Managment System/Client/cliDan.py
+++ Stock Managment System/Client/cliDan.py
@@ -4,4 +4,4 @@ import client
 with open("config.yml", 'r') as cfg:
     config = yaml.load(cfg, Loader=yaml.FullLoader)
 
-client.runClient("192.168.1.110")
+client.runClient("192.168.1.93")
--- Stock Managment System/Client/client.py
+++ Stock Managment System/Client/client.py
@@ -1,23 +1,23 @@
 import socket, sys
 
 def runClient(ipaddr):
-	s = socket.socket()
-	host = ipaddr ## server = 131
-	port = 2100
+    s = socket.socket()
+    host = ipaddr ## server = 131
+    port = 2100
 
-	s.connect((host, port))
+    s.connect((host, port))
 
-	send = sys.argv[1].encode()
-	s.send(send)
+    send = sys.argv[1].encode()
+    s.send(send)
 
-	exitCom = False
+    exitCom = False
 
-	while not exitCom:
-		receive = s.recv(1024).decode()
-		if receive != "":
-			print (receive)
+    while not exitCom:
+        receive = s.recv(1024).decode()
+        if receive != "":
+            print (receive)
 
-		if receive == "EXIT":
-			exitCom = True
+        if receive == "EXIT":
+            exitCom = True
 
-	s.close()
+    s.close()
