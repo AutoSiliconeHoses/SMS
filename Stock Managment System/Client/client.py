426dad3554cfdc0b8ddd09a0470175bb1497e10e
Daniel Wise <daniel@autosiliconehoses.com>
2019-08-05 16:57:16 +0100

    Client updates
    + Now processes queue position and returns it
    + Now processes system arguments and function arguments in a logical manner

--- Stock Managment System/Client/client.py
+++ Stock Managment System/Client/client.py
@@ -1,13 +1,23 @@
-import socket, sys
+import socket, sys, re
 
-def runClient(ipaddr):
+def runClient(ipaddr="192.168.1.131", args=None):
+    if len(sys.argv) > 1:
+        args = sys.argv[1]
+        print(args)
+    elif args is not None:
+        None
+    else:
+        print("Error: No arguments given")
+        return
+
+    queue = None
     s = socket.socket()
     host = ipaddr ## server = 131
     port = 2100
 
     s.connect((host, port))
 
-    send = sys.argv[1].encode()
+    send = args.encode()
     s.send(send)
 
     exitCom = False
@@ -16,8 +26,11 @@ def runClient(ipaddr):
         receive = s.recv(1024).decode()
         if receive != "":
             print (receive)
-
+            if re.match("You are #.* in the queue", receive):
+                search = re.search("(?<=#).*?(?= )", receive)
+                queue = receive[search.start():search.end()]
         if receive == "EXIT":
             exitCom = True
 
     s.close()
+    return queue
