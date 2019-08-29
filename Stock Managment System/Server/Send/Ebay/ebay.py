04412a478155a86598f9d82803600cd6936ae1f7
Daniel Wise <daniel@autosiliconehoses.com>
2019-08-29 17:54:30 +0100

    eBay Progress

--- Stock Managment System/Server/Send/Ebay/ebay.py
+++ Stock Managment System/Server/Send/Ebay/ebay.py
@@ -1,4 +1,5 @@
 import yaml, os, re
+from operator import itemgetter
 
 def Binary_search(L, target):
 	start = 0
@@ -18,22 +19,38 @@ with open("config.yml", 'r') as cfg:
 
 for storecode in config['ebay']:
     if config['ebay'][storecode]['enabled']:
-        print(storecode, "enabled")
+        print(storecode.upper())
         for supplier in config['suppliers']:
             if config['suppliers'][supplier]['enabled']:
                 storePath = os.path.join("Server/Send/Ebay/StoreFiles/", storecode.upper(), supplier+"-FULLSTOCK.csv")
                 if os.path.isfile(storePath):
-                    print("\t", storePath)
-                    # TODO: Check for new file and then binary search them together
-                    
-                    # store = []
-                    # with open() as storeFile:
-                    #     storeLines = sto.readlines()
-                    #     for line in storeLines:
-                    #         store.append(re.split('\t',line.strip()))
+                    stockPath = os.path.join("Server/Send/Ebay/StockFiles/", supplier + ".tsv")
+                    if os.path.isfile(stockPath):
+                        print("\t", supplier, "MATCH")
+                        store = []
+                        with open(storePath) as storeFile:
+                            storeLines = storeFile.readlines()
+                            for line in storeLines:
+                                store.append(re.split(',',line.strip()))
 
-                    # stock = []
-                    # with open() as stockFile:
-                    #     stockLines = stoc.readlines()
-                    #     for line in stockLines:
-                    #         stock.append(re.split('\t',line.strip()))
+                        stock = []
+                        with open(stockPath) as stockFile:
+                            stockLines = stockFile.readlines()
+                            for line in stockLines:
+                                stock.append(re.split('\t',line.strip()))
+
+                        stockSort = sorted(stock, key=itemgetter(0))
+                        stockSkus = [line[0] for line in stockSort]
+
+                        newdata = ""
+                        for storeLine in store:
+                            found = Binary_search(stockSkus,storeLine[5])
+                            if found:
+                                print("\t\t", storeLine[5], "has a match")
+                                # TODO: Add to output data
+                        # TODO: Write data to file
+                        with open('''PUT FILEPATH HERE''', 'w') as txtfile:
+                            txtfile.write(newdata)
+                            txtfile.close()
+
+                        # TODO: Upload to File Exchange
