11c93b07bd08df91279cf592b1267168faac510d
Daniel Wise <daniel@autosiliconehoses.com>
2019-09-09 15:42:29 +0100

    Decco: Fixes, parses and processes
    Finally found that the loose ampersands "&" were what was causing the parsing errors.
    This shouldn't have been something that we should have had to deal with, but its done now.
    
    + Replaces "&" with "&amp;" (How it is supposed to be)
    + Parses data
    + Finds the data and builds it to a list
    + Reads table data and processes with max and min
    + Writes output file to StockFiles

--- Stock Managment System/Suppliers/DECCO/decco.py
+++ Stock Managment System/Suppliers/DECCO/decco.py
@@ -1,9 +1,71 @@
-import os, zipfile, csv
+import os, zipfile, csv, re, yaml
+import xml.etree.ElementTree as ET
 
 def decco():
     print("Starting Decco")
     if os.path.isfile('Server/Receive/Emails/decco.zip'):
+        # Load config data
+        with open("config.yml", 'r') as cfg:
+            config = yaml.load(cfg, Loader=yaml.FullLoader)
+
+        MAX = config['suppliers']['decco']['max']
+        MIN = config['suppliers']['decco']['min']
+
+        # Fetch file and unzip
         with zipfile.ZipFile('Server/Receive/Emails/decco.zip', 'r') as zip_ref:
             zip_ref.extractall('Suppliers/DECCO')
         os.rename('Suppliers/DECCO/barc5-3628270409191600.xls', "Suppliers/DECCO/decco.xml")
-        # TODO: Parse XML despite it being broken
+
+        # Fix Decco file
+        clean = []
+        with open("Suppliers/DECCO/decco.xml", "r") as f:
+            for line in f:
+                cleanline = re.sub(r" *(?=<\/Data>)", "", line)
+                cleanline = re.sub(r'(?<=<Data ss:Type="String">) *', "", cleanline)
+                cleanline = cleanline.replace("&", "&amp;")
+                clean.append(cleanline)
+
+        # Write fixed data to file
+        with open("Suppliers/DECCO/clean.xml", "w") as f:
+            f.writelines(clean)
+            f.close()
+
+        # Find Worksheet node
+        root = ET.parse('Suppliers/DECCO/clean.xml').getroot()
+        for node in root.iter('{urn:schemas-microsoft-com:office:spreadsheet}Worksheet'):
+            if node.attrib['{urn:schemas-microsoft-com:office:spreadsheet}Name'] == 'barc5':
+                ws_node = node
+                break
+
+        # Build table data
+        for table in ws_node.iter('{urn:schemas-microsoft-com:office:spreadsheet}Table'):
+            tabledata = []
+            for row in table.iter('{urn:schemas-microsoft-com:office:spreadsheet}Row'):
+                rowdata = []
+                for cell in row.iter('{urn:schemas-microsoft-com:office:spreadsheet}Cell'):
+                    for data in cell.iter('{urn:schemas-microsoft-com:office:spreadsheet}Data'):
+                        rowdata.append(data.text)
+                tabledata.append(rowdata)
+
+        # Use tabledata to make stockfile
+        vallist = []
+        for row in tabledata[6:-1]:
+            if len(row) >= 16:
+                print(row)
+                sku = str(row[1]+"-DC")
+                stock = int(row[16])
+
+                if stock > MAX:
+                    stock = MAX
+                elif stock < MIN:
+                    stock = 0
+                vallist.append([sku,stock])
+
+        # Write to file
+        with open("Server/Send/StockFiles/decco.tsv", "w", newline="") as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerows(vallist)
+
+        print("Decco Finished")
+    else:
+        print("Decco file missing")
