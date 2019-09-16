ba5a67c169dda870753eaf67846e50c764a4ed48
Daniel Wise <daniel@autosiliconehoses.com>
2019-09-16 18:27:47 +0100

    Schedule/Config Change
    Times and arguments are now date specific which allows for Prime to remain on over the weekends.
    Arguments are now supported in runSchedule.py which essentially means that working supplier files can actually be run.
    
    (This update requires the new format of config.yml)

--- Stock Managment System/Server/runSchedule.py
+++ Stock Managment System/Server/runSchedule.py
@@ -12,20 +12,22 @@ from Client import client as client
 def runSchedule():
     with open("config.yml", 'r') as cfg:
         config = yaml.load(cfg, Loader=yaml.FullLoader)
+    ipaddr = socket.gethostbyname(socket.gethostname())
 
     for s in config['suppliers']:
         if config['suppliers'][s]['enabled'] is True:
             print(s)
             # Loop through days
             for day in config['suppliers'][s]['days']:
-                if config['suppliers'][s]['days'][day] is True:
-                    if config['suppliers'][s]['times'] is not None:
-                        for times in config['suppliers'][s]['times']:
-                            print("\t", day, " @", times)
-                            schedStr = "schedule.every()."+day+".at('"+times+"').do(client.runClient, ipaddr='"+socket.gethostbyname(socket.gethostname())+"', args='RUN sup=["+s+"]')"
+                if config['suppliers'][s]['days'][day]['enabled'] is True:
+                    if config['suppliers'][s]['days'][day]['times'] is not None:
+                        for schedtime in config['suppliers'][s]['days'][day]['times']:
+                            print("\t", day, " @", schedtime)
+                            dests = config['suppliers'][s]['days'][day]['times'][schedtime][0]
+                            opts = config['suppliers'][s]['days'][day]['times'][schedtime][1]
+                            schedStr = "schedule.every()."+day+".at('"+schedtime+"').do(client.runClient, ipaddr='"+ipaddr+"', args='RUN sup=["+s+"] dest=["+dests+"] opt=["+opts+"]')"
                             exec(schedStr)
 
-    # print(schedule.jobs)
     while True:
         schedule.run_pending()
         time.sleep(1)
